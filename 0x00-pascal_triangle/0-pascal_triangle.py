d9fcb359f6bdcbc59c146c8fed1e036dfb4d3309
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-05-30 18:52:36 +0300

    Pascal try 4

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""
-Pascal triangle implementation
-"""
+"""Pascal triangle implementation"""
 
 
 def pascal_triangle(n):
+    """Returns a list of lists of integers representing the Pascal's triangle"""
     if n <= 0:
         return []
 
